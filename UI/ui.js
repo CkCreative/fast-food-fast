17fdb36b974d4f8fd5ad2846a44ec902ba7b4e9d
Mike CK <mikeck93@gmail.com>
2018-09-01 12:33:44 +0300

    Add images to the home page
    The images for the home page added to the js file.

--- UI/ui.js
+++ UI/ui.js
@@ -2,7 +2,7 @@ const items = [
     {
     id : 1,
     title : 'Food Type 1',
-    img : 'https://images.pexels.com/photos/7658/food-pizza-box-chalkboard.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img : 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 100,
     serves: 1,
     description: 'some long description outlining various benefits etc.',
@@ -12,7 +12,7 @@ const items = [
 {
     id : 2,
     title : 'Food Type 2',
-    img : 'https://images.pexels.com/photos/2232/vegetables-italian-pizza-restaurant.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    img : 'https://images.pexels.com/photos/459469/pexels-photo-459469.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 114,
     serves: 3,
     description: 'some long description outlining various benefits etc.',
@@ -22,7 +22,7 @@ const items = [
 {
     id : 3,
     title : 'Food Type 3',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 543,
     serves: 10,
     description: 'some long description outlining various benefits etc.',
@@ -31,7 +31,7 @@ const items = [
 {
     id : 4,
     title : 'Food Type 4',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/46239/salmon-dish-food-meal-46239.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 450,
     serves: 3,
     description: 'some long description outlining various benefits etc.',
@@ -40,7 +40,7 @@ const items = [
 {
     id : 5,
     title : 'Food Type 5',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/1095550/pexels-photo-1095550.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 340,
     serves: 7,
     description: 'some long description outlining various benefits etc.',
@@ -49,7 +49,7 @@ const items = [
 {
     id : 6,
     title : 'Food Type 6',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/247685/pexels-photo-247685.png?auto=compress&cs=tinysrgb&h=350',
     price: 570,
     serves: 4,
     description: 'some long description outlining various benefits etc.',
@@ -58,7 +58,7 @@ const items = [
 {
     id : 7,
     title : 'Food Type 7',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/64208/pexels-photo-64208.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 920,
     serves: 6,
     description: 'some long description outlining various benefits etc.',
@@ -67,7 +67,7 @@ const items = [
 {
     id : 8,
     title : 'Food Type 8',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/8500/food-dinner-pasta-spaghetti-8500.jpg?auto=compress&cs=tinysrgb&h=350',
     price: 220,
     serves: 1,
     description: 'some long description outlining various benefits etc.',
@@ -76,7 +76,7 @@ const items = [
 {
     id : 9,
     title : 'Food Type 9',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 120,
     serves: 3,
     description: 'some long description outlining various benefits etc.',
@@ -85,7 +85,7 @@ const items = [
 {
     id : 10,
     title : 'Food Type 10',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 130,
     serves: 7,
     description: 'some long description outlining various benefits etc.',
@@ -94,7 +94,7 @@ const items = [
 {
     id : 11,
     title : 'Food Type 11',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 135,
     serves: 4,
     description: 'some long description outlining various benefits etc.',
@@ -103,7 +103,7 @@ const items = [
 {
     id : 12,
     title : 'Food Type 12',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/299347/pexels-photo-299347.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 155,
     serves: 2,
     description: 'some long description outlining various benefits etc.',
@@ -112,7 +112,7 @@ const items = [
 {
     id : 13,
     title : 'Food Type 13',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/775031/pexels-photo-775031.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 175,
     serves: 3,
     description: 'some long description outlining various benefits etc.',
@@ -121,7 +121,7 @@ const items = [
 {
     id : 14,
     title : 'Food Type 14',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/629093/pexels-photo-629093.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 195,
     serves: 6,
     description: 'some long description outlining various benefits etc.',
@@ -130,7 +130,7 @@ const items = [
 {
     id : 15,
     title : 'Food Type 15',
-    img : 'https://',
+    img : 'https://images.pexels.com/photos/62097/pexels-photo-62097.jpeg?auto=compress&cs=tinysrgb&h=350',
     price: 275,
     serves: 4,
     description: 'some long description outlining various benefits etc.',
@@ -140,10 +140,10 @@ const items = [
 
 let products = document.getElementById('products')
 
-let cards = items.map(({title, description, serves, price}) => {
+let cards = items.map(({title, description, img, serves, price}) => {
     return `<div class="my-card shaddow-1">
         <div class="card-header">${title}</div>
-        <img src="https://via.placeholder.com/800x400" class="card-img">
+        <img src="${img}" class="card-img">
         <div class="card-desc">${description} <br>Can serve <b>${serves} </b>person(s).</div>
         <div class="card-action"><div class="card-price">Ksh. ${price}</div><p>Order Now</p></div>
     </div>
